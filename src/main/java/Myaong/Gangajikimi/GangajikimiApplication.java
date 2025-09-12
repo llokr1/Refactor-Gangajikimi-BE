6e87c79fd22dae53f635a4f4ca556d0a604ba2d2
llokr1 <ghdwlsrl100@gachon.ac.kr>
2025-09-12 19:40:52 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/Myaong/Gangajikimi/GangajikimiApplication.java
@@ -0,0 +1,13 @@
+package Myaong.Gangajikimi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GangajikimiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GangajikimiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Myaong/Gangajikimi/GangajikimiApplicationTests.java
@@ -0,0 +1,13 @@
+package Myaong.Gangajikimi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GangajikimiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
