960dce15d74d4534f52843d256174f7d2188c65a
llokr1 <ghdwlsrl100@gachon.ac.kr>
2025-09-13 21:47:06 +0900

    프로젝트 초기 세팅

---
+++ src/main/java/Myaong/Gangajikimi/common/response/ErrorCode.java
@@ -0,0 +1,27 @@
+package Myaong.Gangajikimi.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+
+    // COMMON
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "COMMON500", "서버 에러, 관리자에게 문의 바랍니다."),
+    VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "COMMON400", "유효하지 않는 값입니다."),
+
+    // AUTH
+    INVALIDATE_TOKEN(HttpStatus.BAD_REQUEST, "AUTH400", "토큰 정보가 유효하지 않습니다."),
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "AUTH401", "AccessToken 유효 기간이 만료되었습니다."),
+    TOKEN_MISSING(HttpStatus.UNAUTHORIZED, "AUTH402", "토큰을 찾을 수 없습니다."),
+    INVALIDATE_PASSWORD(HttpStatus.UNAUTHORIZED, "AUTH403", "비밀번호가 일치하지 않습니다."),
+
+    // MEMBER
+    MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER400", "존재하지 않는 회원 ID입니다.");
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+}
