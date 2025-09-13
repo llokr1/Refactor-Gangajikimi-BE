add5cebba21c5b1d9ddfed21ce8751f355b6b044
leewonhee-3054 <dldnjsgml3054@naver.com>
2025-09-13 22:09:27 +0900

    push test

---
+++ src/main/java/Myaong/Gangajikimi/config/QueryDSLConfig.java
@@ -0,0 +1,20 @@
+package Myaong.Gangajikimi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.JPQLTemplates;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class QueryDSLConfig {
+	private final EntityManager entityManager;
+	@Bean
+	public JPAQueryFactory jpaQueryFactory() {
+		return new JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager);
+	}
+}
