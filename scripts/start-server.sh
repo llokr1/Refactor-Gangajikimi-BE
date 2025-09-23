af6bd763fa344b7e7ddedde59167f867b2b06267
leewonhee-3054 <dldnjsgml3054@naver.com>
2025-09-23 09:48:43 +0900

    commit5

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -14,14 +14,9 @@ docker rm redis || true
 echo "Redis 컨테이너 실행 중..."
 docker run -d --name redis -p 6379:6379 redis:7
 
-# ECR 로그인 추가 (★ 중요)
-echo "ECR 로그인 중..."
-aws ecr get-login-password --region ap-northeast-2 \
-  | docker login --username AWS --password-stdin 339462084118.dkr.ecr.ap-northeast-2.amazonaws.com
-
-# 4. ECR에서 최신 애플리케이션 이미지 pull
-echo "ECR에서 최신 이미지 Pull 중..."
-docker pull 339462084118.dkr.ecr.ap-northeast-2.amazonaws.com/gangajikimi-server:latest
+# 4. dockerhub 에서 최신 애플리케이션 이미지 pull
+echo "dockerhub 최신 이미지 Pull 중..."
+docker pull dldnjsgml3054/gangajikimi-server:latest
 
 # 5. 새 애플리케이션 컨테이너 실행 (DB 환경변수 직접 주입)
 echo "새 애플리케이션 컨테이너 실행 중..."
@@ -31,6 +26,21 @@ docker run -d --name gangajikimi-server -p 8080:8080 \
   -e SPRING_DATASOURCE_USERNAME=postgres \
   -e SPRING_DATASOURCE_PASSWORD=gangajikimi1234 \
   -e SPRING_DATASOURCE_DRIVER-CLASS-NAME=org.postgresql.Driver \
-  339462084118.dkr.ecr.ap-northeast-2.amazonaws.com/gangajikimi-server:latest
+  -e SPRING_SQL_INIT_MODE=never \
+  -e SPRING_JPA_HIBERNATE_DDL_AUTO=update \
+  -e SPRING_JPA_SHOW_SQL=true \
+  -e SPRING_JPA_PROPERTIES_HIBERNATE_DIALECT=org.hibernate.dialect.PostgreSQLDialect \
+  -e SPRING_DATA_REDIS_HOST=redis \
+  -e SPRING_DATA_REDIS_PORT=6379 \
+  -e SPRING_CACHE_TYPE=redis \
+  -e JWT_ACCESS_SECRETKEY=gangajikimibimilkeyjironggaebalgeumanhagosibda \
+  -e JWT_ACCESS_EXPIRATION=600000 \
+  -e JWT_REFRESH_EXPIRATION=1209600000 \
+  -e LOGGING_LEVEL_ORG_HIBERNATE_TYPE=trace \
+  -e LOGGING_LEVEL_ORG_SPRINGFRAMEWORK_SECURITY=debug \
+  -e SERVER_SERVLET_ENCODING_CHARSET=UTF-8 \
+  -e SERVER_SERVLET_ENCODING_FORCE=true \
+  -e SERVER_SERVLET_ENCODING_ENABLED=true \
+  dldnjsgml3054/gangajikimi-server:latest
 
 echo "--------------- 서버 배포 끝 -----------------"
